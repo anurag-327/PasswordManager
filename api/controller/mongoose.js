6e0f8505724d3b99483a8683c3cf4543e02ac88f
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-02-23 12:50:17 +0530

    v-0.1.1 backend updates

--- api/controller/mongoose.js
+++ api/controller/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose=require("mongoose");
 const dotenv=require('dotenv').config();
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URL)
+
+// mongoose.connect(process.env.MONGO_URL)
+// .then(()=> console.log("Database connected sucessfully"))
+// .catch(err => console.log(err.message));
+
+// 
+mongoose.connect(`mongodb+srv://anurag-327:${process.env.MONGO_ATLAS}@cluster0.wlfzhgd.mongodb.net/PasswordManager?retryWrites=true&w=majority`)
 .then(()=> console.log("Database connected sucessfully"))
-.catch(err => console.log(err.message));
+.catch(err => console.log(err.message))
--- api/server.js
+++ api/server.js
@@ -20,3 +20,5 @@ app.listen(PORT,() =>
 {
     console.log("server running on port",PORT)
 })
+
+module.exports = app;
