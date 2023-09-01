672eff0a7bf5179d76ba7abdf11e9cfcc4a3ad8a
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-09-02 00:25:36 +0530

    Shifted to render hoisting

--- client/src/base.js
+++ client/src/base.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8000"
-export const BASE_URL = "https://passwordmanager-production.up.railway.app"
+export const BASE_URL = "https://vault-qlb9.onrender.com"
 // export const QUICKSIGN_URL="https://localhost:5173"
 export const QUICKSIGN_URL="https://quick-sign.vercel.app"
 export const QUICKSIGN_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE1ZjlmZjMzMTdjZjgxNGE3ZjI5YTYiLCJpYXQiOjE2NzkxNjE4Njd9.G95ddr3telwGEjRZBeVLKxLW4kDAWcKC0uRKC6mTHak"
